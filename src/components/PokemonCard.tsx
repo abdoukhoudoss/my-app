6e945fade907f4e691c152ec7df1f8597dd1742d
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-10-22 17:06:04 +0200

    components2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,26 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
-    return ( 
-    <figure> 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
-     )  
-  }
-  export default PokemonCard;
+    
+    const pokemon = pokemonList[1];
+
+    return (
+        <figure>
+            {pokemon.imgSrc 
+                ? <img src={pokemon.imgSrc} alt={pokemon.name} />
+                : <p>???</p>
+            }
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
+}
+
+export default PokemonCard;
