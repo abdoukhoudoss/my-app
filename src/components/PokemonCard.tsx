d8a915aaba849b38a78f7b9d7c7cb299ac5f473c
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-10-23 14:29:24 +0200

    propos.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,24 @@
-function PokemonCard({ pokemon }) { 
+import React from 'react';
 
+interface Pokemon {
+    imgSrc?: string; 
+    name: string;    
+}
+
+interface PokemonCardProps {
+    pokemon: Pokemon;
+}
+
+const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => { 
     return (
-      <figure>
-        {pokemon.imgSrc 
-          ? <img src={pokemon.imgSrc} alt={pokemon.name} />
-          : <p>???</p>
-        }
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
+        <figure>
+            {pokemon.imgSrc 
+                ? <img src={pokemon.imgSrc} alt={pokemon.name} />
+                : <p>???</p>
+            }
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
     );
-  }
-  
-  export default PokemonCard;
+};
+
+export default PokemonCard;
