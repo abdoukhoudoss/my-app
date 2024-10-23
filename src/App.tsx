f9f58e4841d29e461417d57af15f4266c64f4808
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-10-23 15:12:52 +0200

    statte.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import './App.css'
-import PokemonCard from './components/PokemonCard'
+import './App.css';
+import PokemonCard from './components/PokemonCard';
 
-const pokemonList = [
-  {
-      name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-      name: "mew",
-  },
-];
+import React, { useState } from 'react';
 
+const App = () => {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
-function App() {
+  const pokemonList = [
+      { imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png', name: 'Pikachu' },
+      { imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png', name: 'Bulbasaur' },
+      { imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png', name: 'Charmander' },
+      { imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png', name: 'quirtle'},
+      { imgSrc: '', name:'mew'},
+  ];
+
+  const handlePrevious = () => {
+      setPokemonIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : pokemonList.length - 1));
+  };
+
+  const handleNext = () => {
+      setPokemonIndex((prevIndex) => (prevIndex < pokemonList.length - 1 ? prevIndex + 1 : 0));
+  };
+
   return (
-  <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-      <PokemonCard pokemon={pokemonList[1]} />
-  </div>
+      <div>
+          <h1>Pokémon</h1>
+          <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+          <div>
+              <button onClick={handlePrevious}>Précédent</button>
+              <button onClick={handleNext}>Suivant</button>
+          </div>
+      </div>
   );
-}
-export default App
+};
+
+export default App;
