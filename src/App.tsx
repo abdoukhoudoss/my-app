e3991f027b7db47f825fece6ddd96416340124dc
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-10-24 20:23:33 +0200

    events.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PokemonCard from './components/PokemonCard';
 import NavBar from './components/NavBar';
 
@@ -7,45 +7,49 @@ const pokemonList = [
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
+  },
+  {
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
+  },
+  {
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
+  },
+  {
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
+  },
+  {
       name: "mew",
-    },
-  ];
+  },
+];
 
 const App = () => {
   const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  useEffect(() => {
+    alert("hello pokemon trainer :)");
+  }, []); 
+
   const handlePrevious = () => {
-    setPokemonIndex((prevIndex) => prevIndex - 1);
+    setPokemonIndex((prevIndex) => Math.max(prevIndex - 1, 0)); 
   };
 
   const handleNext = () => {
-    setPokemonIndex((prevIndex) => prevIndex + 1);
+    setPokemonIndex((prevIndex) => Math.min(prevIndex + 1, pokemonList.length - 1));
   };
 
   return (
     <div>
       <h1>Pokemon</h1>
-      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
       <NavBar onPrevious={handlePrevious} onNext={handleNext} pokemonList={pokemonList} setPokemonIndex={setPokemonIndex} />
     </div>
   );
 };
 
 export default App;
-
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,30 @@
 interface NavBarProps {
-  pokemonList: PokemonList[]
-  setPokemonIndex: (index: number) => void
+  pokemonList: PokemonList[];
+  setPokemonIndex: (index: number) => void;
 }
 
 interface PokemonList {
-  name: string,
-  imgSrc?: string
+  name: string;
+  imgSrc?: string;
 }
 
 const NavBar = ({ setPokemonIndex, pokemonList }: NavBarProps) => {
+  const handleClick = (index: number) => {
+    setPokemonIndex(index);
+    if (pokemonList[index].name === "pikachu") {
+      alert("pika pikachu !!!");
+    }
+  };
+
   return (
     <div className="navbar">
       {pokemonList.map((pokemon, index) => (
-        <button key={index} onClick={() => setPokemonIndex(index)}>
+        <button key={index} onClick={() => handleClick(index)}>
           {pokemon.name}
         </button>
       ))}
     </div>
   );
 };
+
 export default NavBar;
